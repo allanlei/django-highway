82ecaf6c8d34ac5832b9ef1c55a2977b53ba6710
Allan Lei <allanlei@helveticode.com>
2012-12-24 15:46:29 +0800

    Add base package files

---
+++ highway/conf.py
@@ -0,0 +1,6 @@
+from appconf import AppConf
+from django.conf import settings
+
+
+class HighwayAppConf(AppConf):
+	pass
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+	name='django-highway',
+	version='0.1',
+	description='',
+	author='Allan Lei',
+	author_email='allanlei@helveticode.com',
+	url='https://www.github.com/allanlei/django-highway',
+	keywords = 'django highway multitenant'.split(),
+	license = 'BSD',
+	packages=find_packages(),
+	install_requires=[
+        'Django>=1.4',
+        'django-appconf>=0.5',
+    ],
+)
